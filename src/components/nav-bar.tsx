4f1a83e77938f358ed9b4ec03d8df65ff297d86f
balkacer <balcacer.enmanuel@gmail.com>
2023-10-04 12:28:21 -0400

    moving some things

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,4 +1,6 @@
 // import { User } from "../mock/user"
+import viteLogo from '/vite.svg'
+import arrowDown from '/images/arrow-down.svg'
 
 export type NavbarProps = {
 
@@ -9,14 +11,14 @@ export default function Navbar({ }: NavbarProps) {
     <header className="flex bg-blue-600 px-8 py-4 w-screen box-border justify-center">
       <div className="flex max-w-[1024px] min-h-full items-center gap-[4rem] w-full">
         {/* Logo */}
-        <div style={{ backgroundImage: "/vite.svg" }} className="h-full aspect-video bg-white rounded-xl"></div>
+        <img src={viteLogo} className="h-full aspect-video bg-white rounded-xl"></img>
         {/* Search Bar */}
         <div className="relative w-full h-full">
           <input type="text" placeholder='Search "javascript developer"' className=" input-md bg-white text-black rounded-full placeholder:text-gray-500 w-full pr-[175px]" />
           <div className="absolute flex h-[70%] top-[15%] right-0 mr-2">
             <button className="h-full text-blue-600 bg-blue-300 flex items-center gap-3 px-4 whitespace-nowrap rounded-lg font-bold top-[15%] right-0 mr-2">
               Categories
-              <div style={{ backgroundColor: '/images/arrow-down.svg' }} className="w-[15px] aspect-square bg-blue-600"></div>
+              <img src={arrowDown} className="w-[15px] aspect-square text-blue-600"></img>
             </button>
             <div className="h-full aspect-square bg-blue-600 rounded-full"></div>
           </div>
